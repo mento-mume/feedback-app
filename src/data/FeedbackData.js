200873d0d2861e41786cfeaf27c1b1cfc15e47bb
Mento Mume <akpankevman@gmail.com>
2022-08-31 16:09:15 +0100

    radio buttons up now

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "This is supposed to ne lorem ipsum dolor dummy text but i did not feel like going to copy from the github. Nothing beats originality anyways",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "This is supposed to ne lorem ipsum dolor dummy text but i did not feel like going to copy from the github. Nothing beats originality anyways",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "This is supposed to ne lorem ipsum dolor dummy text but i did not feel like going to copy from the github. Nothing beats originality anyways",
+  },
+];
+
+export default FeedbackData;
